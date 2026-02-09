5c369e46afbee2e6e0d5d3673616592357411400
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-09 22:18:45 +0200

    Implement the rotation on the ImageCropper

--- src/components/ImageCropper.tsx
+++ src/components/ImageCropper.tsx
@@ -5,9 +5,10 @@ import './ImageCropper.css'
 interface CropperProps {
   image: string;
   onCropComplete: (croppedAreaPixels: any) => void;
+  rotation: number;
 }
 
-export default function ImageCropper({ image, onCropComplete }: CropperProps) {
+export default function ImageCropper({ image, onCropComplete, rotation }: CropperProps) {
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
 
@@ -19,9 +20,11 @@ export default function ImageCropper({ image, onCropComplete }: CropperProps) {
           crop={crop}
           zoom={zoom}
           aspect={4 / 3}
+          rotation={rotation}
           onCropChange={setCrop}
           onCropComplete={(_, pixels) => onCropComplete(pixels)}
           onZoomChange={setZoom}
+          onRotationChange={() => {}}
         />
       </div>
       <div className="controls">
